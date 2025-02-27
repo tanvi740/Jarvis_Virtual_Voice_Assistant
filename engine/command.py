b7512b399c999b9fe38bbb845073d7e5ff96ef91
tanvi740 <tanvigajare72@gmail.com>
2025-02-27 23:19:02 +0530

    day 5 completed

--- engine/command.py
+++ engine/command.py
@@ -1,5 +1,7 @@
+import time
 import pyttsx3
 import eel
+import speech_recognition as sr
 
 def speak(text):
     
@@ -9,7 +11,36 @@ def speak(text):
     engine.setProperty('rate', 174)
     print(voices)
     engine.say(text)
-    
+    #eel.receiverText(text)
     engine.runAndWait()
 
-speak("You Can Do it")
+
+def takecommand():
+
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print('listening....')
+        #eel.DisplayMessage('listening....')
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source)
+        
+        audio = r.listen(source, 10, 6)
+
+    try:
+        print('recognizing')
+        #eel.DisplayMessage('recognizing....')
+        query = r.recognize_google(audio, language='en-in')
+        print(f"user said: {query}")
+        
+        #time.sleep(2)
+       
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return ""
+    
+    return query.lower()
+
+
+text=takecommand()
+speak(text)
