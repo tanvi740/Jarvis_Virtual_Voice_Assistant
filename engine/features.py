1dd1acfed9ef6f69d49c264cec10bff9d299695c
tanvi740 <tanvigajare72@gmail.com>
2025-02-27 14:33:41 +0530

    day 3 Complete

---
+++ engine/features.py
@@ -0,0 +1,7 @@
+from playsound import playsound
+
+#playing the assistant sound function
+def playassistantsound():
+    music_dir = "www/assets/audio/start_sound.mp3"
+    playsound(music_dir)
+   
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import eel
 
+from engine.features import playassistantsound
+playassistantsound()
 eel.init("www")
 os.system('start msedge.exe --app="http://localhost:8000/index.html"')
 eel.start('index.html', mode=None, host='localhost', block=True)
